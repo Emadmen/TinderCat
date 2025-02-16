25f6b11094ebdc2081bfb7e62d41fcb65155fa8e
Emad Menkar <Emad.Menkar@ascent.io>
2025-02-16 21:15:49 +0000

    fix

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
+import HomeScreen from '../screens/Home';
+import ChatScreen from '../screens/Chat';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { View, Text } from 'react-native';
-import { } from 'react-native-gesture-handler';
+import User from '../res/svgs/user';
+import Message from '../res/svgs/message';
+import Cat from '../res/svgs/cat';
+import { colors } from '../res/colors';
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = () => {
-  return (
-    <View><Text>Tinder Cat..</Text></View>
-  )
+const getIconColor = (isFocused: boolean) => {
+  return isFocused ? colors.primary : colors.grey;
 }
 
 export const Navigation = () => {
@@ -16,13 +18,34 @@ export const Navigation = () => {
     <Tab.Navigator screenOptions={{ headerShown: false, tabBarStyle: { elevation: 3, marginBottom: 40, width: 200, alignSelf: 'center', borderRadius: 36 } }}>
       <Tab.Screen
         name="Home"
+        options={{
+          tabBarShowLabel: false, tabBarIcon: ({ focused }) => {
+            return (
+              <Cat color={getIconColor(focused)} />
+            );
+          },
+        }}
         component={HomeScreen}
       />
       <Tab.Screen
         name="Chat"
-        component={HomeScreen} />
+        options={{
+          tabBarShowLabel: false, tabBarIcon: ({ focused }) => {
+            return (
+              <Message color={getIconColor(focused)} />
+            );
+          },
+        }}
+        component={ChatScreen} />
       <Tab.Screen
         name="Profile"
+        options={{
+          tabBarShowLabel: false, tabBarIcon: ({ focused }) => {
+            return (
+              <User color={getIconColor(focused)} />
+            );
+          },
+        }}
         component={HomeScreen} />
     </Tab.Navigator>
   );
